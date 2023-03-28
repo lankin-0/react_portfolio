350f4869a2436d5e2390515140683c8a6312226c
lankin <lanki@mail.com>
2023-03-28 05:39:58 +0200

    Final refactor

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -59,7 +59,7 @@ const Home = () => {
           <img
             src={HeroImage}
             alt="my profile"
-            className="rounded-2xl mx-auto w-2/3 md:w-80 ml-5"
+            className="rounded-2xl mx-auto w-2/3 md:w-80 ml-14 mb-14"
           />
         </div>
       </div>
